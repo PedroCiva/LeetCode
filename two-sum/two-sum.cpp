109d2878972e271e8f2e6f59878ac2ecfa1bf722
Pedro Civa Wiltuschnig <55590822+PedroCiva@users.noreply.github.com>
2021-02-20 20:59:03 -0500

    Time: 8 ms (74.84%), Space: 9 MB (71.94%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //only sum elements smaller than target
+        int num1 = 0;
+        int num2 = 0;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+
+            num1 = nums[i];
+            for (int x = i + 1; x < nums.size(); ++x)
+            {
+
+                num2 = nums[x];
+
+                if ((num1 + num2) == target)
+                {
+                    result.push_back(i);
+                    result.push_back(x);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+};
